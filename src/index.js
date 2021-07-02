e39c724017f20bf4d77faca9c34964c1019939a9
george-shammar <george.gbenle@outlook.com>
2021-07-02 19:17:27 +0000

    Update game ui

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square" onClick={function() {alert('alert');}}>
+        <button className="square" onClick={() => {alert('alert');}}>
           {this.props.value}
         </button>
       );
