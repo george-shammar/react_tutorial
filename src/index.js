d98b97b1bee331fde8d9a8d98b47fd49c8670be2
george-shammar <george.gbenle@outlook.com>
2021-07-05 18:02:13 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -59,12 +59,15 @@ import './index.css';
 
 
     handleClick(i) {
-      const history = this.state.history;    const current = history[history.length - 1];    const squares = current.squares.slice();    if (calculateWinner(squares) || squares[i]) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);   
+      const current = history[history.length - 1];    const squares = current.squares.slice();    if (calculateWinner(squares) || squares[i]) {
         return;
       }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
-        history: history.concat([{        squares: squares,      }]),      xIsNext: !this.state.xIsNext,
+        history: history.concat([{        squares: squares,      }]), 
+        stepNumber: history.length,    
+        xIsNext: !this.state.xIsNext,
       });
     }
 
