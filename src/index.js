f44de73f45d8d1fd71716abceb12a63b192ba740
george-shammar <george.gbenle@outlook.com>
2021-07-05 16:46:27 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-  
   function Square(props) {
     return (
       <button className="square" onClick={props.onClick}>
