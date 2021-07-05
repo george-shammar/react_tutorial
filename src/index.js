5283ec3024d5e27da30ef06a99023100403fb021
george-shammar <george.gbenle@outlook.com>
2021-07-05 17:53:27 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ import './index.css';
         'Go to move #' + move :        
         'Go to game start';      
         return (        
-          <li>          
+          <li key={move}>          
             <button onClick={() => this.jumpTo(move)}>{desc}</button>        
           </li>      
         );    
