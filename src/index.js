61156e97ffee26219645b2f93e8aa055c3ef1213
george-shammar <george.gbenle@outlook.com>
2021-07-05 17:58:03 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -68,6 +68,8 @@ import './index.css';
       });
     }
 
+    jumpTo(step) {    this.setState({      stepNumber: step,      xIsNext: (step % 2) === 0,    });  }
+
     render() {
       const history = this.state.history;    
       const current = history[history.length - 1];    
