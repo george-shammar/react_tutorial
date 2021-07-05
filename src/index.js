a913fe46bc798772be2eaf1033469d65949f761c
george-shammar <george.gbenle@outlook.com>
2021-07-05 17:46:08 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -70,7 +70,19 @@ import './index.css';
     render() {
       const history = this.state.history;    
       const current = history[history.length - 1];    
-      const winner = calculateWinner(current.squares);    
+      const winner = calculateWinner(current.squares); 
+      
+      const moves = history.map((step, move) => {      
+        const desc = move ?        
+        'Go to move #' + move :        
+        'Go to game start';      
+        return (        
+          <li>          
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>        
+          </li>      
+        );    
+      });
+
       let status;    
       if (winner) {      
         status = 'Winner: ' + winner;    
