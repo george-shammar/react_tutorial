9c50a645793c19a1956faf61b96414c3065e7772
george-shammar <george.gbenle@outlook.com>
2021-07-05 17:01:48 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ import './index.css';
       if (calculateWinner(squares) || squares[i]) {      
         return;    
       }
+      
       squares[i] = this.state.xIsNext ? 'X' : 'O';    
       this.setState({
       squares: squares,
